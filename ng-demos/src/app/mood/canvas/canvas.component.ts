634dd85ee5657f4eae5258593ab4bc9882f55fc4
ArnaudDesfontaines <desfontainesarnaud@gmail.com>
2018-08-23 10:46:38 +0200

    TeamMood second test !

---
+++ ng-demos/src/app/mood/canvas/canvas.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {MoodService} from "../mood.service";
+
+@Component({
+  selector: 'app-canvas',
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.css']
+})
+export class CanvasComponent implements OnInit {
+
+  constructor(public service:MoodService) { }
+
+  isHappy(){
+    return this.service.mood ==='happy';
+  }
+
+  isSoso(){
+    return this.service.mood ==='soso';
+  }
+
+  isAngry(){
+    return this.service.mood ==='angry';
+  }
+
+
+  ngOnInit() {
+  }
+
+}
---
+++ ng-demos/src/app/mood/mood.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoodService {
+
+  mood ='happy';
+
+  constructor() { }
+
+  updateMood(mood){
+    this.mood = mood;
+  }
+}
